4867820bada264420774531bcf0a717c1fb97978
Max Base <maxbasecode@gmail.com>
2022-11-07 13:44:56 +0100

    create queue go functions

---
+++ Queue.go
@@ -0,0 +1,55 @@
+package main
+
+// type for queue
+type Queue struct {
+	rear  int
+	front int
+	items []int
+}
+
+// add item to queue
+func (q *Queue) Enqueue(i int) {
+	q.items = append(q.items, i)
+	q.rear++
+}
+
+// remove item from queue
+func (q *Queue) Dequeue() int {
+	if q.rear == q.front {
+		return -1
+	}
+	item := q.items[q.front]
+	q.front++
+	return item
+}
+
+// check if queue is empty
+func (q *Queue) IsEmpty() bool {
+	return q.rear == q.front
+}
+
+// check if queue is full
+func (q *Queue) IsFull() bool {
+	return q.rear == len(q.items)
+}
+
+// get size of queue
+func (q *Queue) Size() int {
+	return q.rear - q.front
+}
+
+// get front item of queue
+func (q *Queue) Front() int {
+	if q.rear == q.front {
+		return -1
+	}
+	return q.items[q.front]
+}
+
+// get rear item of queue
+func (q *Queue) Rear() int {
+	if q.rear == q.front {
+		return -1
+	}
+	return q.items[q.rear-1]
+}
